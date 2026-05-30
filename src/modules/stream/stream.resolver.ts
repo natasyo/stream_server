a96ede25f24d345552c5bda49c8066483e4d384c
Natallia <natasyo23@gmail.com>
2026-05-30 23:47:26 +0300

    feat: stream

--- src/modules/stream/stream.resolver.ts
+++ src/modules/stream/stream.resolver.ts
@@ -1,13 +1,49 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StreamService } from './stream.service';
 import { StreamModel } from '@/src/modules/stream/models/stream.model';
+import { FiltersInput } from '@/src/modules/stream/inputs/filters.input';
+import { Authorization } from '@/src/shared/decorators/auth.decorator';
+import { Authorized } from '@/src/shared/decorators/autorized.decorator';
+import type { User } from '@prisma/client';
+import { ChangeStreamInfoInput } from '@/src/modules/stream/inputs/change-stream-info.input';
+import { UploadParam } from '@/src/shared/decorators/upload.param.decorator';
+import type { FileUpload } from 'graphql-upload-ts';
 
 @Resolver('Stream')
 export class StreamResolver {
 	constructor(private readonly streamService: StreamService) {}
 
 	@Query(() => [StreamModel])
-	public async findAll() {
+	public async findAll(@Args('filters') input: FiltersInput) {
+		return this.streamService.findAll(input);
+	}
+
+	@Query(() => [StreamModel, { name: 'findRandomStreams' }])
+	public async findRandom() {
 		return this.streamService.findAll();
+	}
+
+	@Authorization()
+	@Mutation(() => Boolean, { name: 'changeStreamThumbnail' })
+	public async changeThumbail(
+		@Authorized() user: User,
+		@UploadParam('thumbail') thumbail: FileUpload
+	) {
+		return this.streamService.changeThumbnail(user, thumbail);
+	}
+
+	@Authorization()
+	@Mutation(() => Boolean, { name: 'removeStreamThumbail' })
+	public async removeThumbail(@Authorized() user: User) {
+		return this.streamService.removeThumbail(user);
+	}
+
+	@Authorization()
+	@Mutation(() => Boolean, { name: 'changeStreamInfo' })
+	public async changeInfo(
+		@Authorized() user: User,
+		@Args('data') input: ChangeStreamInfoInput
+	) {
+		return this.streamService.changeInfo(user, input);
 	}
 }
--- src/modules/stream/stream.service.ts
+++ src/modules/stream/stream.service.ts
@@ -1,20 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/src/core/prisma/prisma.service';
 import { FiltersInput } from '@/src/modules/stream/inputs/filters.input';
+import { Prisma, Stream, User } from '@prisma/client';
+import { ChangeStreamInfoInput } from '@/src/modules/stream/inputs/change-stream-info.input';
+import { StorageService } from '@/src/modules/libs/storage/storage.service';
+import { FileUpload, Upload } from 'graphql-upload-ts';
+import sharp from 'sharp';
 
 @Injectable()
 export class StreamService {
-	constructor(private readonly prismaService: PrismaService) {}
+	constructor(
+		private readonly prismaService: PrismaService,
+		private readonly storageService: StorageService
+	) {}
+
+	private findBySearchTermFilter(
+		searchTerm: string
+	): Prisma.StreamWhereInput {
+		return {
+			OR: [
+				{
+					title: {
+						contains: searchTerm,
+						mode: 'insensitive'
+					}
+				},
+				{
+					user: {
+						userName: {
+							contains: searchTerm,
+							mode: 'insensitive'
+						}
+					}
+				}
+			]
+		};
+	}
+
+	private async findByUserId(user: User) {
+		return this.prismaService.stream.findUnique({
+			where: {
+				userId: user.id
+			}
+		});
+	}
 
 	public async findAll(input: FiltersInput = {}) {
 		const { take, skip, searchTerm } = input;
+
+		const whereClause = searchTerm
+			? this.findBySearchTermFilter(searchTerm)
+			: undefined;
+
 		return this.prismaService.stream.findMany({
 			take: take ?? 12,
 			skip: skip ?? 10,
 			where: {
 				user: {
 					isDeactivated: false
-				}
+				},
+				...whereClause
 			},
 			include: {
 				user: {
@@ -24,5 +69,91 @@ export class StreamService {
 				}
 			}
 		});
+	}
+
+	public async findRandom() {
+		const total = await this.prismaService.stream.count({
+			where: {
+				user: {
+					isDeactivated: false
+				}
+			}
+		});
+
+		const randomIndexes = new Set<number>();
+		while (randomIndexes.size < 4) {
+			const randomIndex = Math.floor(Math.random() * total);
+			randomIndexes.add(randomIndex);
+		}
+		const streams = await this.prismaService.stream.findMany({
+			where: {
+				user: {
+					isDeactivated: false
+				}
+			},
+			include: {
+				user: true
+			},
+			take: total,
+			skip: 0
+		});
+		return Array.from(randomIndexes).map(index => streams[index]);
+	}
+
+	public async changeInfo(user: User, input: ChangeStreamInfoInput) {
+		const { title, categoryId } = input;
+		await this.prismaService.stream.update({
+			where: {
+				userId: user.id
+			},
+			data: {
+				title
+			}
+		});
+		return true;
+	}
+
+	public async changeThumbnail(user: User, file: FileUpload) {
+		const stream = await this.findByUserId(user);
+		if (!stream) {
+			throw new NotFoundException('Stream does not found');
+		}
+		if (stream.thumbaiUrl) {
+			await this.storageService.deleteFile(stream.thumbaiUrl);
+		}
+		const chunks: Buffer[] = [];
+		for await (const chunk of file.createReadStream()) {
+			chunks.push(chunk);
+		}
+		const buffer = Buffer.concat(chunks);
+		const fileName = `/streams/${user.userName}.webp`;
+		const isGif = file.filename.endsWith('.gif');
+		const processBuffer = await sharp(buffer, { animated: isGif })
+			.resize(1280, 720)
+			.webp()
+			.toBuffer();
+		await this.storageService.uploadFile(
+			fileName,
+			processBuffer,
+			'webp/image'
+		);
+		await this.prismaService.stream.update({
+			where: { userId: user.id },
+			data: { thumbaiUrl: fileName }
+		});
+		return true;
+	}
+	public async removeThumbail(user: User) {
+		const stream = await this.findByUserId(user);
+		if (!stream) {
+			throw new NotFoundException('Stream does not found');
+		}
+		if (!stream.thumbaiUrl) return;
+		await this.storageService.deleteFile(stream.thumbaiUrl);
+		await this.prismaService.stream.update({
+			where: { userId: user.id },
+			data: { thumbaiUrl: null }
+		});
+		return true;
 	}
 }
